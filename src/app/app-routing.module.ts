c6765be56bc974b24964277c334c0b73694f652a
kalinin <prozaik81-2@yandex.ru>
2021-02-18 14:24:06 +0300

    pages generate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthCheckGuard } from './auth-check.guard';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+import { Page1Component } from './components/page1/page1.component';
+import { Page2Component } from './components/page2/page2.component';
+import { Page3Component } from './components/page3/page3.component';
+import { SigninComponent } from './components/signin/signin.component';
+
+const routes: Routes = [
+  { path: '', component: Page1Component, canActivate: [AuthCheckGuard]},
+  { path: 'page1', component: Page1Component, canActivate: [AuthCheckGuard]},
+  { path: 'page2', component: Page2Component, canActivate: [AuthCheckGuard]},
+  { path: 'page3', component: Page3Component, canActivate: [AuthCheckGuard]},
+  { path: 'signin', component: SigninComponent},
+  { path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page1Component } from './components/page1/page1.component';
+import { Page2Component } from './components/page2/page2.component';
+import { Page3Component } from './components/page3/page3.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component,
+    SigninComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
